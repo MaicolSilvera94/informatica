d872316ba090c3d6802a3f857e7d51e55c15353d
MaicolSilvera94 <maicolsilva1994@gmail.com>
2020-02-25 12:30:02 -0300

    version 12

--- administradores/index.php
+++ administradores/index.php
@@ -11,7 +11,7 @@ include('../funciones/funciones.php');
 <html lang="es_PY">
 <head>
 	<title>Administradores</title>
-  <meta name="theme-color" content="#53A6DC">
+  <meta name="theme-color" content="#1a2d90">
 	<meta name="description" content="">
 	<?php include '../includes/head.php'; ?>
 </head>
